64ddb48048c3037c947c34fd67f20d1d2a64ea75
beleengneo <beeleng562@gmail.com>
2022-07-14 10:29:16 +0800

    gitignore update

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\School homework\Application Security Project\Assignment_new\.venv
+set VIRTUAL_ENV=C:\Users\beeleng\Desktop\aspj
 
 if not defined PROMPT set PROMPT=$P$G
 
