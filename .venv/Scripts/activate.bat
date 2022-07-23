0649960e076ff5e629b03dc2bb4af7091e81f7d0
driedwater <210288D@mymail.nyp.edu.sg>
2022-07-23 15:28:47 +0800

    test

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sabih\Documents\GitHub\aspj\.venv
+set VIRTUAL_ENV=C:\School_homework\Application_Security_Project\Assignment_new\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
