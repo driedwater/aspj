f5a2509a7a258a83f27156c8e1b7e6368f6ab250
cgxh <cgxh125@yahoo.com.sg>
2022-07-25 15:15:26 +0800

    set venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\School_homework\Application_Security_Project\Assignment_new\.venv
+set VIRTUAL_ENV=C:\Users\cgxh1\Y2\AppSec\Assignment\aspj-master\aspj-master\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
