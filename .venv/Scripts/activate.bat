3ea720c2531d738e4770f15d283d574a8a584d42
Sabihah12 <sabihahamir12@gmail.com>
2022-08-15 17:59:36 +0800

    ,

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,6 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
+
 set VIRTUAL_ENV=C:\Users\sabih\Documents\GitHub\aspj\.venv
 
 if not defined PROMPT set PROMPT=$P$G
